7f57c8b95789ba71a0c8f80c967053adc10def72
redant <redant@master9>
2012-02-06 16:45:34 +0100

    Creation of Request configuration

---
+++ entities/src/main/java/fr/univartois/ili/sadoc/configuration/Request.java
@@ -0,0 +1,10 @@
+package fr.univartois.ili.sadoc.configuration;
+
+public abstract class Request {
+	
+	public static final String FIND_IN_SIGNATURE_BY_OWNER = "SELECT s FROM Signature s WHERE s.owner = :owner";
+	public static final String FIND_IN_SIGNATURE_BY_DOCUMENT = "SELECT s FROM Signature s WHERE s.document = :document";
+	public static final String FIND_IN_SIGNATURE_BY_COMPETENCE = "SELECT s FROM Signature s WHERE s.competence = :competence";
+	public static final String FIND_IN_SIGNATURE_BY_CERTIFICATE = "SELECT s FROM Signature s WHERE s.certificate = :certificate";
+
+}
