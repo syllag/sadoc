09d71ec7d85dfe7086d694c9eea714d1bec53bfd
yv <vasseur.yoann@gmail.com>
2012-02-10 16:41:57 +0100

    add annotations for manage transactions with Spring.

--- sadocWeb/src/main/java/fr/univartois/ili/sadoc/sadocweb/spring/WSPrivateImpl.java
+++ sadocWeb/src/main/java/fr/univartois/ili/sadoc/sadocweb/spring/WSPrivateImpl.java
@@ -2,6 +2,9 @@ package fr.univartois.ili.sadoc.sadocweb.spring;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import fr.univartois.ili.sadoc.dao.SignatureDAO;
 import fr.univartois.ili.sadoc.entities.Certificate;
 import fr.univartois.ili.sadoc.entities.Competence;
@@ -10,17 +13,21 @@ import fr.univartois.ili.sadoc.entities.Owner;
 
 public class WSPrivateImpl implements WSPrivate {
 	
+	
+	@Transactional (propagation=Propagation.REQUIRED, readOnly=true)
 	public boolean verifyDocument(Byte[] doc, Certificate certificat,
 			Owner utilisateur) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	@Transactional (propagation=Propagation.REQUIRED, readOnly=true)
 	public List<Document> importDocument(Owner owner) {
 		List<Document> documents = SignatureDAO.findDocumentByOwner(owner);
 		return documents;
 	}
 
+	@Transactional (propagation=Propagation.REQUIRED, readOnly=true)
 	public List<Competence> importCompetences(Document document) {
 		List<Competence> competences = SignatureDAO.findCompetenceByDocument(document);
 		return competences;
