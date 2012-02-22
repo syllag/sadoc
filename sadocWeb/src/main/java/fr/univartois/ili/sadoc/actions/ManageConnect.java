dd647e0304c0863efe0c3095224893472b4897ea
damien.wattiez <damien.wattiez@gmail.com>
2012-02-22 14:54:46 +0100

    add document information after flashing QR code and add acces database for manageConnect

--- sadocWeb/src/main/java/fr/univartois/ili/sadoc/actions/ManageConnect.java
+++ sadocWeb/src/main/java/fr/univartois/ili/sadoc/actions/ManageConnect.java
@@ -1,8 +1,15 @@
 package fr.univartois.ili.sadoc.actions;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 import fr.univartois.ili.sadoc.Form.ManageConnectForm;
+import fr.univartois.ili.sadoc.dao.PersistenceProvider;
+import fr.univartois.ili.sadoc.entities.Owner;
 
 public class ManageConnect extends ActionSupport {
 
@@ -13,14 +20,40 @@ public class ManageConnect extends ActionSupport {
 
 	
 	/**
-	 * forn contains connect
+	 * form contains connect
 	 */
 	private ManageConnectForm connect;
 	
 	public String execute(){
-		System.out.println(connect.getEmail());
-		System.out.println(connect.getPassword());
+		
+		EntityManager em = PersistenceProvider.getEntityManager();
+		TypedQuery<Owner> query;
+		query = em.createQuery(
+				"select o FROM Owner o WHERE o.mail = :mail AND o.password = :password ",
+				Owner.class);
+		query.setParameter("mail", connect.getEmail());
+		query.setParameter("password", connect.getPassword());
+		List<Owner> owner = query.getResultList();
+		
+		// if empty
+		if(owner.size() == 0){
+			return INPUT;
+		}
+		
+		//take information profile
+		
+		
 		return SUCCESS;
 	}
+
+	public ManageConnectForm getConnect() {
+		return connect;
+	}
+
+	public void setConnect(ManageConnectForm connect) {
+		this.connect = connect;
+	}
+	
+	
 	
 }
