6c3e797e60152635c9e3850addb4c1799076f48a
francois.guillain <francois.guillain2@gmail.com>
2012-02-22 17:00:36 +0000

    Add xsd + test class for xml certificate

---
+++ sadocWeb/src/main/java/fr/univartois/ili/sadoc/sadocweb/certificate/MyXMLParserErrorHandler.java
@@ -0,0 +1,55 @@
+package fr.univartois.ili.sadoc.sadocweb.certificate;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * @author francois
+ * 
+ */
+public class MyXMLParserErrorHandler implements ErrorHandler {
+
+	private boolean validate = true;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.ErrorHandler#error(org.xml.sax.SAXParseException)
+	 */
+	public void error(SAXParseException arg0) throws SAXException {
+		System.out.println("ERROR");
+		validate = false;
+		arg0.printStackTrace();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.ErrorHandler#fatalError(org.xml.sax.SAXParseException)
+	 */
+	public void fatalError(SAXParseException arg0) throws SAXException {
+		System.out.println("FATAL ERROR");
+		validate = false;
+		arg0.printStackTrace();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.ErrorHandler#warning(org.xml.sax.SAXParseException)
+	 */
+	public void warning(SAXParseException arg0) throws SAXException {
+		System.out.println("WARNING");
+		validate = false;
+		arg0.printStackTrace();
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isValidate() {
+		return validate;
+	}
+
+}
