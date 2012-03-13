41efe6c5e99139bd8cd62f8055bfefe17310e520
habib <habib.bhs@gmail.com>
2012-03-13 15:32:03 +0100

    add getOwner client WebService

--- sadocWeb/src/main/java/fr/univartois/ili/sadoc/client/webservice/IClientWebService.java
+++ sadocWeb/src/main/java/fr/univartois/ili/sadoc/client/webservice/IClientWebService.java
@@ -8,5 +8,5 @@ public interface IClientWebService {
 	public fr.univartois.ili.sadoc.client.webservice.tools.Document getDocument(long idDoc);
 	public Map<fr.univartois.ili.sadoc.client.webservice.tools.Owner, List<fr.univartois.ili.sadoc.client.webservice.tools.Competence>> getCompetences(long idDoc);
 	public fr.univartois.ili.sadoc.client.webservice.tools.Owner createOwner(String firstName ,String lastName, String mail);
-	
+	public fr.univartois.ili.sadoc.client.webservice.tools.Owner getOwner(String mail);
 }
