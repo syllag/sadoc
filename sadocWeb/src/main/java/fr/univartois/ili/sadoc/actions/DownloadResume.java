efd155905ebc07816c72cf7ca4fe60bc4ae764d7
redant <redant@master9>
2012-03-14 17:40:30 +0100

    Create cv.pdf (empty) and download, OKAY Miss get in database the information of resume

--- sadocWeb/src/main/java/fr/univartois/ili/sadoc/actions/DownloadResume.java
+++ sadocWeb/src/main/java/fr/univartois/ili/sadoc/actions/DownloadResume.java
@@ -1,10 +1,20 @@
 package fr.univartois.ili.sadoc.actions;
 
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 
+
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.opensymphony.xwork2.ActionSupport;
+
 
 public class DownloadResume  extends ActionSupport{
 	
@@ -29,12 +39,35 @@ public class DownloadResume  extends ActionSupport{
 		return fileInputStream;
 	}
  
-	public String execute() throws Exception {
-	    fileInputStream = new FileInputStream(new File("/tmp/cv.pdf"));
+	public String execute() {
+		
+		Document document=new Document();
+		FileOutputStream fo;
+		try {
+			fo = new FileOutputStream("/tmp/cvTEST.pdf");
+
+		
+		PdfWriter.getInstance(document,fo);
+		
+		Paragraph prefaceNom = new Paragraph("Coucou je test la création d'un pdf et le download ! ! !");
+		
+		document.open();
+		document.add(prefaceNom);
+		document.close();
+		
+	    fileInputStream = new FileInputStream(new File("/tmp/cvTEST.pdf"));
 	    
 	    System.out.println("id cv : "+cv);
 	    
 	    return SUCCESS;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return ERROR;
+		} catch (DocumentException e) {
+			e.printStackTrace();
+			return ERROR;
+		}
 	}
 	
 }
