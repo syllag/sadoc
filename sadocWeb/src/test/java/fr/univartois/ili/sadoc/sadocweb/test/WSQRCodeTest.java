fb8d20c025d0c02b00a19c1354b89112b396f69f
francois.guillain <francois.guillain2@gmail.com>
2012-03-15 14:02:26 +0000

    Add QRCode test + update properties QRcode

---
+++ sadocWeb/src/test/java/fr/univartois/ili/sadoc/sadocweb/test/WSQRCodeTest.java
@@ -0,0 +1,31 @@
+package fr.univartois.ili.sadoc.sadocweb.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.awt.image.BufferedImage;
+
+import org.junit.Test;
+
+import fr.univartois.ili.sadoc.sadocweb.qrcode.qrcodeGeneration.QRCodeReaderManager;
+import fr.univartois.ili.sadoc.sadocweb.qrcode.qrcodeGeneration.QRCodeWriterManager;
+
+public class WSQRCodeTest {
+
+	/**
+	 * Test encoding  and decoding data
+	 */
+	@Test
+	public void testEncodeAndDecode() {
+		String data = "id=AZERTY0123456789";
+		
+		// data encoding
+		BufferedImage testbi = QRCodeWriterManager.getInstance().encode(data).getImage();
+		
+		// data decoding
+		String data2 = QRCodeReaderManager.getInstance().decodeImage(testbi);
+		
+		// compare String
+		assertTrue(data2.equals(data));
+	}
+	
+}
