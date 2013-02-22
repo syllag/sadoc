d3c72333778d986862023de125371d0f37ae9dbb
christophe delbruyere <c.delbruyere@gmail.com>
2013-02-22 11:05:11 +0100

    Certificate classes to represent different certificates (PGP, X509 & PublicKey) To complete ...

---
+++ sadoc-parent/sadoc-metier-parent/sadoc-metier-ws/src/main/java/fr/univartois/ili/sadoc/metier/ws/certificate/CertificatePublicKey.java
@@ -0,0 +1,39 @@
+package fr.univartois.ili.sadoc.metier.ws.certificate;
+
+import java.security.PublicKey;
+import java.util.Date;
+
+import fr.univartois.ili.sadoc.dao.entities.CertificateType;
+
+public class CertificatePublicKey implements BaseCertificate {
+
+	private PublicKey key;
+	
+	
+	
+	public CertificatePublicKey(PublicKey key) {
+		this.key = key;
+	}
+
+	public CertificatePublicKey() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Date getDateValidity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PublicKey getPublicKey() {
+		return key;
+	}
+
+	@Override
+	public CertificateType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
