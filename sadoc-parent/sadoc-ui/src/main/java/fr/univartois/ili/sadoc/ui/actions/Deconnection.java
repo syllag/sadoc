ae9872a6668c8fcc5e2d9c45581a2884cb88c894
Mouloud Goutal <mouloud.goutal@gmail.com>
2013-03-04 11:29:21 +0100

    change action Deconnexion.java

--- sadoc-parent/sadoc-ui/src/main/java/fr/univartois/ili/sadoc/ui/actions/Deconnection.java
+++ sadoc-parent/sadoc-ui/src/main/java/fr/univartois/ili/sadoc/ui/actions/Deconnection.java
@@ -6,23 +6,23 @@ import org.apache.struts2.interceptor.SessionAware;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-
 public class Deconnection extends ActionSupport implements SessionAware {
 
 	private static final long serialVersionUID = 1L;
 	private Map<String, Object> session;
 
-	public String execute () {
+	public String execute() {
 		session = ActionContext.getContext().getSession();
-		
+
 		session.clear();
 		return SUCCESS;
 	}
-	
-	public void setSession(Map<String, Object> session){
-		  session = this.getSession();
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 	}
-	public Map<String, Object> getSession(){
+
+	public Map<String, Object> getSession() {
 		return session;
 	}
 }
