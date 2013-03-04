1d31fc65521dccb8fc5c12755b6d3d694f3a597c
Mathieu Waroquet <mwaroque@gmail.com>
2013-03-04 14:38:00 +0100

    merge

--- sadoc-parent/sadoc-metier-parent/sadoc-metier-ws/src/main/java/fr/univartois/ili/sadoc/metier/ws/services/IMetierWSServices.java
+++ sadoc-parent/sadoc-metier-parent/sadoc-metier-ws/src/main/java/fr/univartois/ili/sadoc/metier/ws/services/IMetierWSServices.java
@@ -10,40 +10,109 @@ import fr.univartois.ili.sadoc.metier.ws.vo.Signature;
 
 /**
  * @author Alan Delahaye <alan.delahayee@gmail.com>
+ * 
+ */
+/**
+ * @author mathieu15
  *
  */
 public interface IMetierWSServices {
-    //TODO JavaDoc
-	//TODO voir doublons méthode avec partie ui
+	// TODO JavaDoc
+	// TODO voir doublons méthode avec partie ui
 	// Les méthodes DAO de la partie ui devrait devenir
-	// quasi inexistante et tout doit se faire par 
+	// quasi inexistante et tout doit se faire par
 	// appel webservice
-	
+
+	/**
+	 * @param competence
+	 */
 	void createCompetance(Competence competence);
-	
+
+	/**
+	 * @param id
+	 * @return Document
+	 */
 	Document findDocumentById(int id);
-	
+
+	/**
+	 * @param document
+	 * @return Owner
+	 */
 	Owner findOwnerByDocument(Document document);
 
-	List<Competence> findCompetenceByDocument();
+
+	/**
+	 * @param document
+	 * @return List<Competence>
+	 */
+	List<Competence> findCompetencesByDocument(Document document);
+
 	
+	/**
+	 * @param owner
+	 * @return List<Document>
+	 */
 	List<Document> findDocumentByOwner(Owner owner);
+
 	
+	/**
+	 * @param mail
+	 * @return Owner
+	 */
 	Owner findOwnerByMail(String mail);
+
 	
+	/**
+	 * @param owner
+	 * @return void
+	 */
 	void createOwner(Owner owner);
-	
+
+	/**
+	 * @param owner
+	 * @return void
+	 */
 	void updateOwner(Owner owner);
+
 	
+	/**
+	 * @param document
+	 * @return void
+	 */
 	void createDocument(Document document);
+
 	
-	Competence findCompetenceByAcronym(String acronym);
+	/**
+	 * @param acronym
+	 * @return Competence
+	 */
 	
+	Competence findCompetenceByAcronym(String acronym); 
+	
+	
+	/**
+	 * @param owner
+	 * @return Certificate
+	 */
 	Certificate findCertificateByOwner(Owner owner);
+
 	
+	/**
+	 * @param signature
+	 * @return void
+	 */
 	void createSignature(Signature signature);
+
 	
+	/**
+	 * @param document
+	 * @return void
+	 */
 	void updateDocument(Document document);
-	
-	List<Certificate> findCertificateByOwner();
+
+	/**
+	 * @param owner
+	 * @return List<Certificate>
+	 */
+	List<Certificate> findCertificatesByOwner(Owner owner);
 }
