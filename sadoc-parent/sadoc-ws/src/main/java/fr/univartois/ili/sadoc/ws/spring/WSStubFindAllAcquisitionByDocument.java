1f998f04b2e82bc20530b1fee2c278ef90650480
Mathieu Waroquet <mwaroque@gmail.com>
2013-03-18 15:52:44 +0100

    Add implementation of find all acquisition by document. Add multiples files for limited conflicts. Files to be merged.

---
+++ sadoc-parent/sadoc-ws/src/main/java/fr/univartois/ili/sadoc/ws/spring/WSStubFindAllAcquisitionByDocument.java
@@ -0,0 +1,137 @@
+package fr.univartois.ili.sadoc.ws.spring;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import fr.univartois.ili.sadoc.metier.ws.vo.Acquisition;
+import fr.univartois.ili.sadoc.metier.ws.vo.Certificate;
+import fr.univartois.ili.sadoc.metier.ws.vo.Competence;
+import fr.univartois.ili.sadoc.metier.ws.vo.Document;
+import fr.univartois.ili.sadoc.metier.ws.vo.Owner;
+//TODO Replace DAO
+@Endpoint
+public class WSStubFindAllAcquisitionByDocument {
+
+	@Resource(name = "wsPrivate")
+	private WSPrivate wsPrivate;
+	
+	@Resource(name = "wsPublic")
+	private WSPublicFindAllAcquisitionByDocument wsPublic;
+	
+
+	@PayloadRoot(localPart = "createOwnerRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public CreateOwnerResponse createOwner(@RequestPayload CreateOwnerRequest request)
+			throws Exception {
+		CreateOwnerResponse resp = new CreateOwnerResponse();
+		Owner o = wsPublic.createOwner(request.getLastName(),
+				request.getFirstName(), request.getMail());
+		resp.setOwner(o);
+		return resp;
+	}
+
+	@PayloadRoot(localPart = "getOwnerRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public Owner getOwner(@RequestPayload GetOwnerRequest request)
+			throws Exception {
+		return wsPublic.getOwner(request.getMail());
+	}
+
+	@PayloadRoot(localPart = "signDocumentRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public SignDocumentResponse signDocument(@RequestPayload SignDocumentRequest request) {
+		byte[] byteDoc = wsPublic.signDocument(request.getDoc(), request.getName(),
+				request.getOwner(), request.getCompetence());
+		SignDocumentResponse resp = new SignDocumentResponse();
+		resp.setDoc(byteDoc);
+		return resp;
+	}
+
+	public byte[] signDocument(byte[] doc, String name, Certificate certificat,
+			Competence[] competence) {
+		//TODO ???
+		// Cette méthode devrait-elle renvoyer un tableau vide plutôt que null ?
+		return null;
+	}
+
+	@PayloadRoot(localPart = "createCertificateRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public void createCertificate(
+			@RequestPayload CreateCertificateRequest request) {
+		wsPublic.createCertificate(request.getOwner());
+	}
+
+	public List<Certificate> getCertificate(Owner utilisateur) {
+		//TODO ???
+		return null;
+	}
+
+	@PayloadRoot(localPart = "verifyDocumentRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public boolean verifyDocument(@RequestPayload Byte[] doc,
+			@RequestPayload Document document, @RequestPayload Owner utilisateur) {
+		return wsPrivate.verifyDocument(doc, document, utilisateur);
+	}
+
+	@PayloadRoot(localPart = "getDocumentInformationsRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public GetDocumentInformationsResponse getDocumentInformations(
+			@RequestPayload GetDocumentInformationsRequest request) {
+		GetDocumentInformationsResponse getDocumentInformationsResponse = new GetDocumentInformationsResponse();
+		
+		Map<Owner, List<Competence>> lites = wsPrivate
+				.getDocumentInformations(request.getIdDocument());
+		lites.keySet();
+		if (!lites.isEmpty()) {
+			Map.Entry<Owner, List<Competence>> entry = lites.entrySet()
+					.iterator().next();
+
+			getDocumentInformationsResponse.setOwner(entry.getKey());
+
+			getDocumentInformationsResponse.setCompetence(entry
+					.getValue());
+		}
+		
+		return getDocumentInformationsResponse;
+
+	}
+
+	@PayloadRoot(localPart = "importDocumentRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public ImportDocumentResponse importDocument(
+			@RequestPayload ImportDocumentRequest request) {
+		List<Document> dd = wsPrivate.importDocument(request.getOwner());
+		ImportDocumentResponse importDocumentResponse = new ImportDocumentResponse();
+		importDocumentResponse.setDocument(dd);
+		return importDocumentResponse;
+	}
+
+	@PayloadRoot(localPart = "importCompetencesRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public List<Competence> importCompetences(@RequestPayload Document document) {
+		return wsPrivate.importCompetences(document);
+	}
+							  
+	@PayloadRoot(localPart = "getDocumentRequest", namespace = "http://sadoc.com/ac/schemas")
+	@ResponsePayload
+	public GetDocumentResponse getDocument(
+			@RequestPayload GetDocumentRequest request) {
+		
+		Document d = wsPrivate.getDocument(request.getIdDocument());
+		GetDocumentResponse get = new GetDocumentResponse();
+		get.setDocument(d);
+		return get;
+	}
+	
+	public List<Acquisition> findAllAcquisition(Document document)
+	{
+		return wsPublic.findAllAcquisition(document);
+	}
+}
