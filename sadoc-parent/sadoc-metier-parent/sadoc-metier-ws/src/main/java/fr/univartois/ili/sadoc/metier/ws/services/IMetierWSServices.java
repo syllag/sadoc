0f85a12ec823db8bbafaa8ef328b3d74caf259d0
Emeric Blatier <emericb.ps@gmail.com>
2013-03-18 16:30:49 +0100

    Change implementation of MetierWSServices to use interfaces and erase useless methods

--- sadoc-parent/sadoc-metier-parent/sadoc-metier-ws/src/main/java/fr/univartois/ili/sadoc/metier/ws/services/IMetierWSServices.java
+++ sadoc-parent/sadoc-metier-parent/sadoc-metier-ws/src/main/java/fr/univartois/ili/sadoc/metier/ws/services/IMetierWSServices.java
@@ -2,11 +2,8 @@ package fr.univartois.ili.sadoc.metier.ws.services;
 
 import java.io.FileInputStream;
 import java.util.List;
-import java.util.Map;
-
 import fr.univartois.ili.sadoc.metier.ws.vo.Acquisition;
 import fr.univartois.ili.sadoc.metier.ws.vo.Certificate;
-import fr.univartois.ili.sadoc.metier.ws.vo.Competence;
 import fr.univartois.ili.sadoc.metier.ws.vo.Document;
 import fr.univartois.ili.sadoc.metier.ws.vo.Owner;
 import fr.univartois.ili.sadoc.metier.ws.vo.Signature;
@@ -16,17 +13,17 @@ import fr.univartois.ili.sadoc.metier.ws.vo.Signature;
  * 
  */
 public interface IMetierWSServices {
-	// TODO JavaDoc
 	// TODO voir doublons méthode avec partie ui
 	// Les méthodes DAO de la partie ui devrait devenir
 	// quasi inexistante et tout doit se faire par
 	// appel webservice
-
-	// XXX Plus nécessaire car pas Competence dans la BDws
-//	/**
-//	 * @param competence
-//	 */
-//	void createCompetence(Competence competence);
+	
+	/**
+	 * Create an acquisition
+	 * 
+	 * @param acquisition
+	 */
+	void createAcquisition(Acquisition acquisition);
 	
 	/**
 	 * Create a document
@@ -37,7 +34,7 @@ public interface IMetierWSServices {
 	void createDocument(Document document);
 	
 	/**
-	 * 
+	 * Create a ownerWS
 	 * 
 	 * @param owner
 	 * @return void
@@ -51,13 +48,13 @@ public interface IMetierWSServices {
 	 * @return void
 	 */
 	void createSignature(Signature signature);
-
+	
 	/**
-	 * Create an acquisition
+	 * Update an acquisition
 	 * 
 	 * @param acquisition
 	 */
-	void createAcquisition(Acquisition acquisition);
+	void updateAcquisition(Acquisition acquisition);
 	
 	/**
 	 * Update a document
@@ -70,18 +67,8 @@ public interface IMetierWSServices {
 	 * Update an owner
 	 * 
 	 * @param owner
-	 *  
 	 */
 	void updateOwnerWS(Owner owner);
-	
-	/**
-	 * Update an acquisition
-	 * 
-	 * @param acquisition
-	 *  
-	 */
-	void updateAcquisition(Acquisition acquisition);
-	
 
 	/**
 	 * Find a specific document
@@ -90,57 +77,46 @@ public interface IMetierWSServices {
 	 * @return Document
 	 */
 	Document findDocumentById(long id);
-
-	/**
-	 * @param document
-	 * @return Owner
-	 */
-	Owner findOwnerByDocument(Document document);
-
-
 	
 	/**
+	 * Find all the documents of a owner
+	 * 
 	 * @param owner
 	 * @return List<Document>
 	 */
 	List<Document> findDocumentByOwner(Owner owner);
 
+	/**
+	 * Find the owner of a document
+	 * 
+	 * @param document
+	 * @return Owner
+	 */
+	Owner findOwnerByDocument(Document document);
 	
 	/**
+	 * Find a owner with his mail
+	 * 
 	 * @param mail
 	 * @return Owner
 	 */
 	Owner findOwnerByMail(String mail);
 	
 	/**
+	 * Find the certificate of the owner
+	 * 
 	 * @param owner
 	 * @return Certificate
 	 */
 	Certificate findCertificateByOwner(Owner owner);
 
 	/**
+	 * Find all the certificates of the owner
+	 * 
 	 * @param owner
 	 * @return List<Certificate>
 	 */
 	List<Certificate> findCertificatesByOwner(Owner owner);
-	
-	/**
-	 * @param acronym
-	 * @return Competence
-	 */
-	Competence findCompetenceByAcronym(String acronym); 
-	
-	/**
-	 * @param document
-	 * @return List<Competence>
-	 */
-	List<Competence> findCompetencesByDocument(Document document);
-	
-	/**
-	 * 
-	 * @return
-	 */
-	Map<Owner, List<Competence>> findOwnerAndCompetencesByDocumentId(int documentId);
 	
 	/**
 	 * Find an acquisition by acronym (nb:  format idReferentiel:idDomaine:idCompetence:idItem)
