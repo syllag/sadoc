6f3e216293c0dbf3453ef1f56a06778d397bfe37
Marc Leborgne <leborgneprojetsm2@gmail.com>
2013-03-18 16:48:23 +0100

    Add a string spliter which could match empty elements (the String.split() method skip empty elements)

---
+++ sadoc-parent/sadoc-metier-parent/sadoc-metier-commun/src/main/java/fr/univartois/ili/sadoc/metier/commun/utils/StringSplitter.java
@@ -0,0 +1,31 @@
+package fr.univartois.ili.sadoc.metier.commun.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSplitter {
+
+	public static String[] split(String string, char delim){
+		List<String> res=new ArrayList<String>();
+
+		int i=0;
+		int j=0;
+		
+		char[] decoupChar=string.toCharArray();
+		StringBuilder tmp=new StringBuilder();
+		
+		while(i<decoupChar.length){
+			if(decoupChar[i]!=delim)
+				tmp.append(decoupChar[i]);
+			else{
+				res.add(tmp.toString());
+				tmp=new StringBuilder();
+			}
+			i++;
+		}
+		
+		res.add(tmp.toString());
+		
+		return res.toArray(new String[res.size()]);
+	}
+}
