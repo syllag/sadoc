aa06a37a94189ae0dbe479661b35e895a007520b
anicet bart <anicet.bart@gmail.com>
2013-03-21 07:46:15 +0100

    Merge same actions ShowCreateResume and ManageResume into one action ManageResume

--- sadoc-parent/sadoc-ui/src/main/java/fr/univartois/ili/sadoc/ui/actions/ManageResume.java
+++ sadoc-parent/sadoc-ui/src/main/java/fr/univartois/ili/sadoc/ui/actions/ManageResume.java
@@ -1,53 +1,48 @@
 package fr.univartois.ili.sadoc.ui.actions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-import fr.univartois.ili.sadoc.metier.ui.services.IMetierUIServices;
 import fr.univartois.ili.sadoc.metier.ui.vo.Owner;
-import fr.univartois.ili.sadoc.ui.utils.ContextFactory;
+import fr.univartois.ili.sadoc.metier.ui.vo.Resume;
+import fr.univartois.ili.sadoc.ui.utils.Connection;
 
-public class ManageResume extends ActionSupport implements SessionAware,
-		ServletRequestAware {
+public class ManageResume extends ActionSupport implements SessionAware {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 6796507791071667351L;
+	
+	private List<Resume> resumes = new ArrayList<Resume>();
 	private Map<String, Object> session;
-	private HttpServletRequest request;
-	private IMetierUIServices metierUIServices = ContextFactory.getContext()
-			.getBean(IMetierUIServices.class);
-	public String execute() throws Exception {
-		String mail = (String) session.get("mail");
-		if (mail == null) {
+	
+	@Override
+	public String execute() {
+		Owner owner = Connection.getUser(session);
+		if (owner == null) {
 			return "index";
 		}
-		Owner owner = metierUIServices.findOwnerByEmail(mail);
-		request.setAttribute("resumes",owner.getResumes());
+		resumes = owner.getResumes();
 		return SUCCESS;
 	}
 
-	public void setSession(Map<String, Object> arg0) {
-		this.session = arg0;
+	public List<Resume> getResumes() {
+		return resumes;
 	}
 
-	private Map<String, Object> getSession() {
-		return this.session;
+	public void setResumes(List<Resume> listResumes) {
+		this.resumes = listResumes;
 	}
 
-	/**
-	 * @return the metierUIServices
-	 */
-	public IMetierUIServices getMetierUIServices() {
-		return metierUIServices;
+	public Map<String, Object> getSession() {
+		return session;
 	}
 
-	public void setServletRequest(HttpServletRequest request) {
-		this.request = request;
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;	
 	}
-
 }
