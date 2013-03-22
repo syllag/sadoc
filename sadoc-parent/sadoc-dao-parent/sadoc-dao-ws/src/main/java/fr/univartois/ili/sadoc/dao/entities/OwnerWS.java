a7ddc5ce17754eae561fd8fc52181ae3b2626837
sergent.tjmg <sergent.tjmg@gmail.com>
2013-03-22 15:36:27 +0100

    Correct UI and WS for interaction !

--- sadoc-parent/sadoc-dao-parent/sadoc-dao-ws/src/main/java/fr/univartois/ili/sadoc/dao/entities/OwnerWS.java
+++ sadoc-parent/sadoc-dao-parent/sadoc-dao-ws/src/main/java/fr/univartois/ili/sadoc/dao/entities/OwnerWS.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -13,6 +14,7 @@ public class OwnerWS implements Serializable{
 	private static final long serialVersionUID = 68838356125074929L;
 	
 	@Id
+	@GeneratedValue
 	private long id;
 	private String mail_initial;
 	
